c8daccf3699caf4124d7912c0c5a13dad96ae4d3
M.Dzaki <72814435+MHMMDDzaki@users.noreply.github.com>
2024-02-04 22:16:30 +0700

    js day 2

---
+++ task1/script.js
@@ -0,0 +1,13 @@
+let FavoriteBook1 = "Mushoku Tensei";
+const FavoriteBook2 = "Tsuki ga michibiku Isekai Dochuu";
+
+FavoriteBook1 = "The Beginning After The End";
+// FavoriteBook2 = "Mushoku Tensei"
+
+let concat = FavoriteBook1 + ", " + FavoriteBook2;
+
+if(FavoriteBook1 === FavoriteBook2) {
+ console.log("true")
+} else if(FavoriteBook1 !== FavoriteBook2) {
+ console.log("false")
+}
---
+++ task2/script.js
@@ -0,0 +1,14 @@
+let FavoriteBook1 = "Mushoku Tensei";
+const FavoriteBook2 = "Tsuki ga michibiku Isekai Dochuu";
+
+let price1 = 240000
+let price2 = 620000
+
+let mean = (240000 + 620000) / 2
+
+if(mean <= 500000) {
+ console.log("Cheap")
+} else {
+ console.log("Expensive")
+}
+
