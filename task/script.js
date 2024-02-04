a790852a70a32dab8d1a8116a492b64a1b868273
M.Dzaki <72814435+MHMMDDzaki@users.noreply.github.com>
2024-02-04 22:26:47 +0700

    js day 4

--- task/script.js
+++ task/script.js
@@ -1,20 +1,36 @@
 const book = {
- title: "beginning after the end",
- price: 10000,
+  title: "beginning after the end",
+  price: 10000,
+};
+
+function purchaseBook(value, discount, tax, amountStock, amountPurchased) {
+  let booksPurchased = 0;
+
+  for (let i = 0; i < amountPurchased; i++) {
+    if (i === amountStock) {
+      break;
+    }
+    booksPurchased += value.price;
+  }
+
+  console.log("Total Price = " + booksPurchased);
+  let totalDiscount = (booksPurchased * discount) / 100;
+  console.log("amount of discount = " + totalDiscount);
+  let price = booksPurchased - totalDiscount;
+  console.log("price after discount = " + price);
+
+  let totalTax = (price * tax) / 100;
+  console.log("amount of tax = " + totalTax);
+  let priceFinal = price - totalTax;
+  console.log("price after tax = " + priceFinal);
+
+  if (amountPurchased >= amountStock) {
+    console.log("book is currently sold cannot be purchased again");
+  } else {
+    console.log("book can be purchased again");
+  }
+
+  return priceFinal;
 }
 
-function purchaseBook(value, discount, tax) {
- let totalDiscount = value.price * discount / 100
- console.log("amount of discount = " + totalDiscount)
- let price = value.price - totalDiscount
- console.log("price after discount = " + price)
-
- let totalTax = price * tax / 100
- console.log("amount of tax = " + totalTax)
- let priceFinal = price - totalTax
- console.log("price after tax = " + priceFinal)
-
- return priceFinal
-}
-
-purchaseBook(book, 30, 10)
+purchaseBook(book, 30, 10, 4, 4);
