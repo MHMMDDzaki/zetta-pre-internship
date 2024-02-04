ad04c3c62fd222f46fb4cffeaf1a0ae43b2d03f8
M.Dzaki <72814435+MHMMDDzaki@users.noreply.github.com>
2024-02-04 22:13:48 +0700

    js day 1

---
+++ script.js
@@ -0,0 +1,25 @@
+let FavoriteBook1 = "Mushoku Tensei";
+let foo = 3;
+console.log(typeof(foo));
+const FavoriteBook2 = "Tsuki ga michibiku Isekai Dochuu";
+
+
+FavoriteBook1 = "The Beginning After The End";
+// FavoriteBook2 = "Mushoku Tensei"
+
+foo = "three";
+console.log(typeof(foo));
+foo = true
+console.log(typeof(foo));
+
+let concat = FavoriteBook1 + ", " + FavoriteBook2;
+
+let FavoriteBook = {
+  title: "Mushoku Tensei",
+};
+
+console.log(FavoriteBook.title);
+
+FavoriteBook.title = "The Beginning After The End";
+
+console.log(FavoriteBook.title);
