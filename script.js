2e6b73c6aa18ceaf9f85bcbe06977ba6c2e6cafe
M.Dzaki <72814435+MHMMDDzaki@users.noreply.github.com>
2024-02-15 21:15:19 +0700

    javascript day 8

---
+++ script.js
@@ -0,0 +1,333 @@
+const userData = [
+  {
+    first_name: "Varik",
+    last_name: "Tovandy",
+    birth_date: "1995-08-20",
+    address: [
+      {
+        street: "Jl. Wampu",
+        village: "Kwala Bingai",
+        sub_district: "Stabat",
+        city: "Langkat",
+        province: "Sumatera Utara",
+        postal_code: "20811",
+      },
+      {
+        street: "Jl. Cempaka Putih Tengah 19 No. 2",
+        village: "Cempaka Putih",
+        sub_district: "Jakarta Pusat",
+        city: "Jakarta",
+        province: "DKI Jakarta",
+        postal_code: "16235",
+      },
+    ],
+  },
+  {
+    first_name: "Juan",
+    last_name: "Azhar",
+    birth_date: "1995-07-20",
+    address: [
+      {
+        street: "Perumahan Bumi Adipura",
+        village: "Rancabolang",
+        sub_district: "Gedebage",
+        city: "Bandung",
+        province: "Jawa Barat",
+        postal_code: "54321",
+      },
+    ],
+  },
+  {
+    first_name: "âbdi",
+    last_name: "Ramdani",
+    birth_date: "1990-12-05",
+    address: [
+      {
+        street: "Jl. Batununggal Gg. Sukamulya II No.1",
+        village: "Mengger",
+        sub_district: "Bandung Kidul",
+        city: "Bandung",
+        province: "Jawa Barat",
+        postal_code: "40267",
+      },
+      {
+        street: "Jl. Kertajaya No.139",
+        village: "Airlangga",
+        sub_district: "Gubeng",
+        city: "Surabaya",
+        province: "Jawa Timur",
+        postal_code: "60286",
+      },
+    ],
+  },
+  {
+    first_name: "Siti",
+    last_name: "Fatimah",
+    birth_date: "1985-06-25",
+    address: [
+      {
+        street: "Jl. Kagok No.161",
+        village: "Wonotingal",
+        sub_district: "Candisari",
+        city: "Semarang",
+        province: "Jawa Tengah",
+        postal_code: "50252",
+      },
+      {
+       street: "Jl. Kalilangse No.591",
+       village: "Gajahmungkur",
+       sub_district: "Gajahmungkur",
+       city: "Semarang",
+       province: "Jawa Tengah",
+       postal_code: "50232",
+     },
+     {
+      street: "Jl. Jenderal Ahmad Yani No.172",
+      village: "Pekan Binjai",
+      sub_district: "Binjai Kota",
+      city: "Binjai",
+      province: "Sumatera Utara",
+      postal_code: "20713",
+    },
+    ],
+  },
+  {
+    first_name: "Ahmad",
+    last_name: "Hakim",
+    birth_date: "1992-03-10",
+    address: [
+      {
+        street: "Jl. Kalilangse No.591",
+        village: "Gajahmungkur",
+        sub_district: "Gajahmungkur",
+        city: "Semarang",
+        province: "Jawa Tengah",
+        postal_code: "50232",
+      },
+      {
+        street: "Jl. Ireda No.123B",
+        village: "Keparakan",
+        sub_district: "Mergangsan",
+        city: "Yogyakarta",
+        province: "DI Yogyakarta",
+        postal_code: "55121",
+      },
+    ],
+  },
+  {
+    first_name: "Irvin",
+    last_name: "Tovandy",
+    birth_date: "1983-09-15",
+    address: [
+      {
+        street: "Jl. Sutan Syahrir No.829",
+        village: "Ilir",
+        sub_district: "Ilir Timur II",
+        city: "Palembang",
+        province: "Sumatera Selatan",
+        postal_code: "30116",
+      },
+    ],
+  },
+  {
+    first_name: "Rudi",
+    last_name: "Santoso",
+    birth_date: "1998-11-30",
+    address: [
+      {
+        street: "Jl. Bintang No.61 A",
+        village: "Pusat Pasar",
+        sub_district: "Medan Kota",
+        city: "Medan",
+        province: "Sumatera Utara",
+        postal_code: "20211",
+      },
+      {
+        street: "Perumahan Villa Azhar No.C-3",
+        village: "Ulu",
+        sub_district: "Seberang Ulu II",
+        city: "Palembang",
+        province: "Sumatera Selatan",
+        postal_code: "30111",
+      },
+    ],
+  },
+  {
+    first_name: "Ans",
+    last_name: "Barrung",
+    birth_date: "2000-05-20",
+    address: [
+      {
+        street: "Jl. Urip Sumoharjo No.26E",
+        village: "Paropo",
+        sub_district: "Panakkukang",
+        city: "Makassar",
+        province: "Sulawesi Selatan",
+        postal_code: "90233",
+      },
+    ],
+  },
+  {
+    first_name: "Budi",
+    last_name: "Tanjaya",
+    birth_date: "1993-07-12",
+    address: [
+      {
+        street: "Gang 6 No.42",
+        village: "Pelita",
+        sub_district: "Samarinda Utara",
+        city: "Samarinda",
+        province: "Kalimantan Timur",
+        postal_code: "75523",
+      },
+      {
+        street: "Jl. Gajah Mada No.15",
+        village: "Benua Melayu Darat",
+        sub_district: "Pontianak Selatan",
+        city: "Pontianak",
+        province: "Kalimantan Barat",
+        postal_code: "78243",
+      },
+      {
+        street: "Jl. Gang Belakang No.59",
+        village: "Kranggan",
+        sub_district: "Semarang Tengah",
+        city: "Semarang",
+        province: "Jawa Tengah",
+        postal_code: "50139",
+      },
+    ],
+  },
+  {
+    first_name: "Nina",
+    last_name: "Surya",
+    birth_date: "1996-02-28",
+    address: [
+      {
+        street: "Jl. Siaga 3",
+        village: "Pengambangan",
+        sub_district: "Banjarmasin Timur",
+        city: "Banjarmasin",
+        province: "Kalimantan Selatan",
+        postal_code: "70321",
+      },
+      {
+        street: "Jl. Jagalan No.38",
+        village: "Gabahan",
+        sub_district: "Semarang Tengah",
+        city: "Semarang",
+        province: "Jawa Tengah",
+        postal_code: "50135",
+      },
+      {
+        street: "Jl. Jenderal Ahmad Yani No.172",
+        village: "Pekan Binjai",
+        sub_district: "Binjai Kota",
+        city: "Binjai",
+        province: "Sumatera Utara",
+        postal_code: "20713",
+      },
+    ],
+  },
+  {
+    first_name: "Charissa",
+    last_name: "Han",
+    birth_date: "1987-10-08",
+    address: [
+      {
+        street: "Jl. Pinang Ranti II No.1",
+        village: "Makasar",
+        sub_district: "Jakarta Timur",
+        city: "Jakarta",
+        province: "DKI Jakarta",
+        postal_code: "13560",
+      },
+      {
+        street: "Jl. Simongan No.129",
+        village: "Bongsari",
+        sub_district: "Semarang Barat",
+        city: "Semarang",
+        province: "Jawa Tengah",
+        postal_code: "50148",
+      },
+    ],
+  },
+];
+
+function searchUsers(query) {
+  query = query.toLowerCase();
+
+  return userData.filter((user) => {
+    const fullName = `${user.first_name.toLowerCase()} ${user.last_name.toLowerCase()}`;
+    const fullNameWithoutSpaces = fullName.replace(/\s/g, ""); 
+    const searchRegex = new RegExp(`(?=.*${query.split("").join(")(?=.*")}).*`,"i"); 
+    return (
+      searchRegex.test(fullName) || searchRegex.test(fullNameWithoutSpaces)
+    );
+  });
+}
+
+function sortUsers(type, order) {
+  if (type === "birthdate") {
+    return userData.slice().sort((a, b) => {
+      const dateA = new Date(a.birth_date);
+      const dateB = new Date(b.birth_date);
+      if (order === "asc") {
+        return dateA - dateB;
+      } else if (order === "dsc") {
+        return dateB - dateA;
+      } else {
+        throw new Error("Invalid order parameter");
+      }
+    });
+  } else if (type === "city") {
+    if (order === "asc") {
+      userData.forEach((user) => {
+        user.address.sort((a, b) => a.city.localeCompare(b.city));
+        user.firstCity = user.address[0].city;
+      });
+      const sortedUsers = userData.slice().sort((a, b) => {
+        return a.firstCity.localeCompare(b.firstCity);
+      });
+      return sortedUsers;
+    } else if (order === "desc") {
+      userData.forEach((user) => {
+        user.address.sort((a, b) => b.city.localeCompare(a.city));
+        user.firstCity = user.address[0].city;
+      });
+      const sortedUsers = userData.slice().sort((a, b) => {
+        return b.firstCity.localeCompare(a.firstCity);
+      });
+      return sortedUsers;
+    } else {
+      throw new Error("Invalid type parameter");
+    }
+  }
+}
+
+function findUsersByTotalAddresses(users, totalAddresses) {
+  return users.filter((user) => user.address.length === totalAddresses);
+}
+
+// console.log("Sorted Users by City and Address:");
+// sortUsers("city", "desc").forEach((user) => {
+//   console.log(`${user.first_name} ${user.last_name}`);
+//   // console.table(user.address)
+//   user.address.forEach((address) => {
+//     console.log(
+//       `  ${address.street}, ${address.village}, ${address.sub_district}, ${address.city}, ${address.province} ${address.postal_code}`
+//     );
+//   });
+//   console.log();
+// });
+// console.log(sortUsers("city", "asc"));
+
+// Example: Find users with 1 address
+// console.log("Users");
+// findUsersByTotalAddresses(userData, 3).forEach((user) => {
+//   console.log(`${user.first_name} ${user.last_name}`);
+// });
+
+console.log("Search Results:", searchUsers("ni ab"));
+// console.log(userData.length)
+
